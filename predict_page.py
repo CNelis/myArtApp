37bcb20239a937f8f43fff5857907c3a9767dccc
CNelis <christopher.nelis@yahoo.com>
2021-08-10 15:59:14 +0100

    path test

--- predict_page.py
+++ predict_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd86b47976032492a2464115cd1196ca047b5badff2acb9ecaf0cc52d420ae60
-size 2713
+oid sha256:d342381d83a5f0af63a68d5ee226731fbd2766d923abca5fab9863d05ca1c500
+size 2704
