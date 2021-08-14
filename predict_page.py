dfd9e7fec0d4d8b897f89d57303cd2794c86a88a
CNelis <christopher.nelis@yahoo.com>
2021-08-14 15:29:33 +0100

    print colour test

--- predict_page.py
+++ predict_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85bba2423c8fd15a67e0a031980a339372c020bf47112b630e36c0f2f3c5ed30
-size 3312
+oid sha256:1bb95043b5ad1378bec9758af16731e83472a092f0e890dd0ae8cf96ed39a43f
+size 3456
