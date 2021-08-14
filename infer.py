cd998852ec41232adc075f6bb1f66211fbd9b554
CNelis <christopher.nelis@yahoo.com>
2021-08-14 18:32:16 +0100

    style number 4 = rustic

--- infer.py
+++ infer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc571ff6dd6fee8f9a138e880aa0388d7fa96ec0a512d1b122e75e5cbf4f9500
-size 4552
+oid sha256:6a443219a08a129421001f81f592fae5887580168ec53327db2df6f9b6249f92
+size 4578
