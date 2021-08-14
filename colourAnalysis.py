66a2d4e5a249e1b40326ff4a1144359339262e1f
CNelis <christopher.nelis@yahoo.com>
2021-08-14 18:53:33 +0100

    remove plt functions

--- colourAnalysis.py
+++ colourAnalysis.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6338774cad5094c50020429b0512741e293ed65a70b219c9460a2a910015dce2
-size 2340
+oid sha256:c5c16d7a9553eb67401dbb2debdc044d90739a8b37f66f57cd5fc887ccdefcae
+size 2362
