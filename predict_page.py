f632a3568bd425c082d5c03b8db3cc2c7a11790d
CNelis <christopher.nelis@yahoo.com>
2021-08-14 20:40:03 +0100

    hint added, info

--- predict_page.py
+++ predict_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d163c4b27dfa0493eecc37d2eb869054e8295d6d69c301ee062750aae6a2043
-size 4635
+oid sha256:e8d1895009e9026d2b001067d63549d353aab714e124b1d175d683ff6d5114be
+size 4515
