f876f51bc2f9e934a55b5d969b6f2a3a390eac4f
CNelis <christopher.nelis@yahoo.com>
2021-08-14 14:12:02 +0100

    colour dataframe

--- infer.py
+++ infer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe31059d1916e8ca4010504c952a59ee52c592e05038103d6ecdd4ea5b5eeb89
-size 4519
+oid sha256:65061cdcb063c6fdf4f5b0d5929c3b9e950d90629a5d7159d2cb847bd788e55f
+size 4576
