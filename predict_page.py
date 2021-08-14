e4c16f48e35badc2a8cfd1d95e618bdf62fac9e2
CNelis <christopher.nelis@yahoo.com>
2021-08-14 18:17:25 +0100

    .title method

--- predict_page.py
+++ predict_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fba37daa790d457360c3f744818df107cfea3843fb39e5be6c630a3513e2a36e
-size 4358
+oid sha256:06bb6d26e46036881fac816686345b4ee9504066054acf6684e8e64fc4093ebf
+size 4379
