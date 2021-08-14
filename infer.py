6830c8f5a32ab39b0739e2871976007c1f2fe8b7
CNelis <christopher.nelis@yahoo.com>
2021-08-14 14:42:22 +0100

    return label added to inferstyle

--- infer.py
+++ infer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8fa01524d708fcab2cbf52f6622c434a82e94657be7aa7b17ce3c82d053450f
-size 4619
+oid sha256:1eaae5239bcdaffa5dc391eb72235bcb56776c3a0356db5548db1aceece53f82
+size 4552
--- predict_page.py
+++ predict_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fa82e52f9eea8273dd9a40771697440e3f10f55a2711619af5a4b729761c337
-size 3106
+oid sha256:c0c6848b2f446ce3a343f0c61b9ffab72bdf2087e49e0304b3af45e16c3c317e
+size 3080
