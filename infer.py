5e71352c84bd8480ca94d4c819d8619d5fddbdba
CNelis <christopher.nelis@yahoo.com>
2021-08-15 00:15:21 +0100

    markers

--- infer.py
+++ infer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cd1cc468f785dbdac1fd6eed84df16f47843aeeeb9e8a09c912f7edab1f79fa
-size 4622
+oid sha256:01f70777849a771ac67d09c51e8d6c4cbe3b1d5b4d29c590c336d9a1ee16be74
+size 4682
--- predict_page.py
+++ predict_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38f947def2d1c85879da711a36d66a2f2efa671257db7f785ca4ac9f7bff8726
-size 4644
+oid sha256:3630c512d372334eb52f3016858aec3f1db83698f9c8b9a02b3947d159750b1a
+size 4665
