5de79523c8791cf8b81025fb1e2a21e397c2d7c8
CNelis <christopher.nelis@yahoo.com>
2021-08-14 18:12:17 +0100

    painting info + minor updates

--- colourAnalysis.py
+++ colourAnalysis.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14dd841c4cf4185b097153b6fe1a8e44b75da9efc6a40fcba8c791ae69b63113
-size 2650
+oid sha256:5da86372bca681cb3a00329654ff713dd9ea21011363bddb74a186a3d48e3fcc
+size 2054
--- predict_page.py
+++ predict_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b870bec49619141593d772d0bc4f9214ccda528c4e95b2bc07463af92909bc56
-size 4110
+oid sha256:fba37daa790d457360c3f744818df107cfea3843fb39e5be6c630a3513e2a36e
+size 4358
