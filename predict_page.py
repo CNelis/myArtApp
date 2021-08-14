9c90201a9b4d35545087e064d77d1b1f179c30a6
CNelis <christopher.nelis@yahoo.com>
2021-08-14 15:03:40 +0100

    direct type change

--- predict_page.py
+++ predict_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12e9724f275a44a4b2585817de7a3ce46451b76323c5f92ce3334f99eb252912
-size 3284
+oid sha256:4d8301aaa4a35d20b6ad3638a38d816ecbe2c6fceb94c0fcb1de704c65b473d3
+size 3289
