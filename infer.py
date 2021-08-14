7d769a316d57da7fc014f6a3a91598d307fdcc19
CNelis <christopher.nelis@yahoo.com>
2021-08-14 14:26:28 +0100

    change return type to series

--- infer.py
+++ infer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3edef7a0d005aa1602b592477c6d2720c56cff78c477b5acdd61c299b1d00696
-size 4611
+oid sha256:09fbb957796189a1c5fb60c4cacd4394f0f4debe4008de2627f39d898c8f4f78
+size 4608
