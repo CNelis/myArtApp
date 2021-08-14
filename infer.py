5d9f53fe24cf772dde93f07f7dc5760cc1658752
CNelis <christopher.nelis@yahoo.com>
2021-08-14 21:08:33 +0100

    updated models

--- infer.py
+++ infer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1798f5a7d43b634f16851b07f4246ca65f066b31e18b605df0b9c4f0339c7c67
-size 4553
+oid sha256:2a517c1111ae3174af2da5161f118ba3ce8a130b4ee3c46f7d029312065e2d09
+size 4558
--- predict_page.py
+++ predict_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af2702361d27318373ab163b4e24514f31ec7a94cfd28fd6a17fb63d87d7d583
+oid sha256:38f947def2d1c85879da711a36d66a2f2efa671257db7f785ca4ac9f7bff8726
 size 4644
