1851fc00a12c12e8531874b53781d39fd22d9c8c
CNelis <christopher.nelis@yahoo.com>
2021-08-15 00:01:38 +0100

    catch error

--- infer.py
+++ infer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5de6ca0bcc1f1c07e5b12a161ec3709485a1ee235d24be47f8d3cb972f372be
-size 4558
+oid sha256:9cd1cc468f785dbdac1fd6eed84df16f47843aeeeb9e8a09c912f7edab1f79fa
+size 4622
