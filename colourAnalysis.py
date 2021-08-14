60e9248189b32db29e3bc90cbb90bef655a36ea7
CNelis <christopher.nelis@yahoo.com>
2021-08-14 20:14:11 +0100

    reduce image size, using pillow

--- colourAnalysis.py
+++ colourAnalysis.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ca5d5e7b35435405e2f8bbbe5355797e27bf3a3ce5907fd3bc287b95f2a6cbf
-size 2573
+oid sha256:78be383343d269fd57eb267beabcba11459150fe8fbaa0ddf5d20b02fa8530d9
+size 2575
