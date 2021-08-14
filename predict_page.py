b2209f5f08da1b4e7169e271726a84176bb52563
CNelis <christopher.nelis@yahoo.com>
2021-08-14 15:00:45 +0100

    removed loc from query

--- predict_page.py
+++ predict_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccb3b4d1785c97dbce53d29092ed1762125a14eb4381b8d50fbe71fe0d112c0f
-size 3288
+oid sha256:12e9724f275a44a4b2585817de7a3ce46451b76323c5f92ce3334f99eb252912
+size 3284
