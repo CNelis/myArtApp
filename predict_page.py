82cfecb8e794d2b180e6a76ba13787ad7d3115a6
CNelis <christopher.nelis@yahoo.com>
2021-08-14 16:58:43 +0100

    different way to show image

--- predict_page.py
+++ predict_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0a39d319029d93268aee207f4bea5c4a1d276156fe04cd5b7b1f12c8bb411f9
-size 4139
+oid sha256:e87811971845eb5760e96e1c01e78cdd474f26a59c4f601307db41e3e785aaf4
+size 4144
