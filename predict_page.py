fd4d97f47154533a7417d380663328feaa19903e
CNelis <christopher.nelis@yahoo.com>
2021-08-14 15:06:09 +0100

    trying boolean indexing for query

--- predict_page.py
+++ predict_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d8301aaa4a35d20b6ad3638a38d816ecbe2c6fceb94c0fcb1de704c65b473d3
-size 3289
+oid sha256:53a0143642e85c6ef02554682eae540501aac6a8c97026fabf6d8e5b0816f301
+size 3284
