ac5a0a99c195d8ad98c6f93591d78eb231b410c6
CNelis <christopher.nelis@yahoo.com>
2021-08-15 00:23:31 +0100

    test

--- infer.py
+++ infer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e3e40690aab4a44cff3670ed5357405bc9ac574b444b9250a14c01fef4af673
-size 4704
+oid sha256:71034bb3aa8aa1cafb0c20b4b047513562265154c4b7b51cbedae4b4578531e1
+size 4691
