572cc146572a760a9f12fa2924c56630259c54ec
CNelis <christopher.nelis@yahoo.com>
2021-08-14 14:36:50 +0100

    removed colour dataframe

--- infer.py
+++ infer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db65f3b8586efbce548c71972ddfec7bb93efdbc123cf7c059ce61ccad07862b
-size 4657
+oid sha256:19e58b45963d2319175644f6eb1a4b1d101e182c950eeb9336cd411ff71b2148
+size 4584
