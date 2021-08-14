3b0ee24f1ccb665863e960aacedacf144c57acf1
CNelis <christopher.nelis@yahoo.com>
2021-08-14 20:59:30 +0100

    hint css

--- predict_page.py
+++ predict_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7932aacf1eced10d82daee5e94938d6e42fd16456e3e0cec6e2a7cff8fc5274
-size 4632
+oid sha256:a5b5f47cf7841476cb35dc06e8c2ed3c32f44476c619622a8c23d4a5231e0851
+size 4635
