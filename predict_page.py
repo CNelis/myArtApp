10a3addfcd01ee14a41eacc17a4db82fbd7110d8
CNelis <christopher.nelis@yahoo.com>
2021-08-14 15:16:11 +0100

    added loc

--- predict_page.py
+++ predict_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4aa864681b77f63ecbf76c04d7d1c6d6c2d8f18e156caf2e7ccd5a0e30dc8ef
-size 3286
+oid sha256:ed1265aced78835cf78c21e0b4c5bda73779817b282c5032beee2ae4baf396bc
+size 3290
