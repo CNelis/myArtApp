709a071563d734fa7a6fc18658052c1c91782e54
CNelis <christopher.nelis@yahoo.com>
2021-08-18 23:07:11 +0100

    version with about page

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b942ad6da3225f4387a4647ace9c6c6c6ee8ae80b9dbcacb701286844faf4f9a
-size 379
+oid sha256:14f82b28f2e2257d1a583c64b452b36eb5fc1277f48c2525fc04fb920ba96f8b
+size 298
