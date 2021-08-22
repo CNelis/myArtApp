4da647bb7b08e515e629a7c5cfae4cefa6a63b7a
CNelis <christopher.nelis@yahoo.com>
2021-08-23 00:01:36 +0100

    version with about page

--- about_page.py
+++ about_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ab59ea436f2570e10e6a47c6cad5f8c4781071a1e311373b40bc21164b01dce
-size 1279
+oid sha256:eafb1941b55fb48c192ec556f891de5f75830a5bcaaae15dcd521685ba26d892
+size 1281
